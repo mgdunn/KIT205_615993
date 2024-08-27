0b21e7c275379b4e7d7ba507a8e9f46d3d9f67d9
mgdunn <139691015+mgdunn@users.noreply.github.com>
2024-08-28 01:48:26 +1000

    Add files via upload

---
+++ Week2/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "List.h"
+
+int main() {
+    //Call the list_test function
+    list_adhoc_test();
+
+    // Return 0 to indicate successful completion
+    return 0;
+}
